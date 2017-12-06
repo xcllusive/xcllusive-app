b13219cbcefdc73ce5513b762d060802c8593c83
Cayo Bayestorff <bayesrox@gmail.com>
2017-12-06 12:15:45 +1100

    Created Business Edit Page

---
+++ src/pages/BusinessEditPage/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {} from 'semantic-ui-react';
+
+class BusinessEditPage extends Component {
+  render() {
+    return <div>oi</div>;
+  }
+}
+
+export default BusinessEditPage;
