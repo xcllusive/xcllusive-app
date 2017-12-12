71d3a53f69a73381fe1f86d922a4682bc8df0c5b
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2017-12-12 00:25:08 -0200

    add styles

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  primaryColorDark: '#00796B',
+  primaryColor: '#009688',
+  primaryColorLight: '#B2DFDB',
+  primaryColorText: '#F7F7F7',
+  accentColor: '#00BCD4',
+  primaryTextColor: '#212121',
+  secondaryTextColor: '#757575',
+  dividerColor: '#BDBDBD'
+};
