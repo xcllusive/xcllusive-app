1cbac93798bce57778609f4e39ad4cc54800cb29
Cayo Bayestorff <bayesrox@gmail.com>
2018-02-07 14:31:00 +1100

    included formik in NewUser

--- src/pages/SystemSettings/NewUserModal.js
+++ src/pages/SystemSettings/NewUserModal.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Modal, Form, Icon, Checkbox } from 'semantic-ui-react'
+import { Modal, Form, Icon, Checkbox, Label } from 'semantic-ui-react'
+import { withFormik } from 'formik'
 import styled from 'styled-components'
+import Yup from 'yup'
 
-const officeRegion = [
-  { key: '1', text: 'Sydney Office', value: '1' },
-  { key: '2', text: 'Melbourne Office', value: '2' }
+const dataRegion = [
+  { key: '1', text: 'Sydney Office', value: 'Sydney Office' },
+  { key: '2', text: 'Melbourne Office', value: 'Melbourne Office' },
+  { key: '3', text: 'Gosford Office', value: 'Gosford Office' },
+  { key: '4', text: 'Cowra Office', value: 'Cowra Office' },
+  { key: '5', text: 'Camberra Office', value: 'Camberra Office' }
 ]
 
 const listingAgent = [
@@ -20,13 +25,16 @@ const userType = [
 ]
 
 const state = [
-  { key: '1', text: 'SDA', value: '1' },
-  { key: '2', text: 'ASD', value: '2' }
+  { key: '1', text: 'NSW', value: 'NSW' },
+  { key: '2', text: 'QLD', value: 'QLD' },
+  { key: '3', text: 'SA', value: 'SA' },
+  { key: '4', text: 'TAS', value: 'TAS' },
+  { key: '5', text: 'VIC', value: 'VIC' },
+  { key: '6', text: 'WA', value: 'WA' }
 ]
 
 const CheckboxFormatted = styled.div`
-  padding-right: 1em
-`
+  padding-right: 1em`
 
 class NewUserModal extends Component {
   constructor (props) {
@@ -50,39 +58,162 @@ class NewUserModal extends Component {
   }
 
   render () {
+    const {
+      values,
+      touched,
+      errors,
+      handleChange,
+      handleBlur
+    } = this.props
+
     return (
       <Modal open={this.props.modalOpen}>
         <Modal.Header align='center'> {this.props.user !== undefined ? `${this.props.user.firstName} ${this.props.user.lastName}` : 'New User'}</Modal.Header>
         <Modal.Content>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group widths='equal'>
-              <Form.Input required label='Username' value={this.props.user.userName} />
-              <Form.Input required label='Password' value={this.props.user.password} />
+              <Form.Field>
+                <Form.Input
+                  label='Email'
+                  name='email'
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.email && touched.email && <Label basic color='red' pointing content={errors.email} />}
+              </Form.Field>
+              <Form.Field>
+                <Form.Input
+                  type='password'
+                  label='Password'
+                  name='password'
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.password && touched.password && <Label basic color='red' pointing content={errors.password} />}
+              </Form.Field>
+              <Form.Field>
+                <Form.Input
+                  label='First Name'
+                  name='firstName'
+                  value={values.firstName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.firstName && touched.firstName && <Label basic color='red' pointing content={errors.firstName} />}
+              </Form.Field>
+              <Form.Field>
+                <Form.Input
+                  label='Last Name'
+                  name='lastName'
+                  value={values.lastName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.lastName && touched.lastName && <Label basic color='red' pointing content={errors.lastName} />}
+              </Form.Field>
             </Form.Group>
             <Form.Group widths='equal'>
-              <Form.Input required label='First Name' value={this.props.user.firstName} />
-              <Form.Input required label='Last Name' value={this.props.user.lastName} />
-              <Form.Input required label='Email' value={this.props.user.email} />
+              <Form.Field>
+                <Form.Input
+                  label='Home Phone'
+                  name='phoneHome'
+                  value={values.phoneHome}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.phoneHome && touched.phoneHome && <Label basic color='red' pointing content={errors.phoneHome} />}
+              </Form.Field>
+              <Form.Field>
+                <Form.Input
+                  label='Work Phone'
+                  name='phoneWork'
+                  value={values.phoneWork}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.phoneWork && touched.phoneWork && <Label basic color='red' pointing content={errors.phoneWork} />}
+              </Form.Field>
+              <Form.Field>
+                <Form.Input
+                  label='Mobile Phone'
+                  name='phoneMobile'
+                  value={values.phoneMobile}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.phoneMobile && touched.phoneMobile && <Label basic color='red' pointing content={errors.phoneMobile} />}
+              </Form.Field>
             </Form.Group>
             <Form.Group widths='equal'>
-              <Form.Input label='Home Phone' value={this.props.user.phoneHome} />
-              <Form.Input label='Work Phone' value={this.props.user.phoneWork} />
-              <Form.Input label='Mobile Phone' value={this.props.user.phoneMobile} />
+              <Form.Field>
+                <Form.Select
+                  label='State'
+                  name='state'
+                  options={state}
+                  value={values.state}
+                />
+              </Form.Field>
+              <Form.Field>
+                <Form.Input
+                  label='Suburb'
+                  name='suburb'
+                  value={values.suburb}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.suburb && touched.suburb && <Label basic color='red' pointing content={errors.suburb} />}
+              </Form.Field>
+              <Form.Field>
+                <Form.Input
+                  label='Street'
+                  name='street'
+                  value={values.street}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.street && touched.street && <Label basic color='red' pointing content={errors.street} />}
+              </Form.Field>
+              <Form.Field>
+                <Form.Input
+                  label='Post Code'
+                  name='postCode'
+                  value={values.postCode}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.postCode && touched.postCode && <Label basic color='red' pointing content={errors.postCode} />}
+              </Form.Field>
             </Form.Group>
             <Form.Group widths='equal'>
-              <Form.Select
-                label='State'
-                options={state}
-                value={this.props.user.state}
-              />
-              <Form.Input label='Suburb' value={this.props.user.suburb} />
-              <Form.Input label='Street' value={this.props.user.street} />
-              <Form.Input label='Post Code' value={this.props.user.postCode} />
-            </Form.Group>
-            <Form.Group widths='equal'>
-              <Form.Select required label='Office Region' options={officeRegion} value={this.props.user.dataRegion} />
-              <Form.Select required label='Listing Agent' options={listingAgent} value={this.props.user.listingAgent} />
-              <Form.Select required label='User Type' options={userType} value={this.props.user.userTypeId} />
+              <Form.Field>
+                <Form.Select
+                  label='Office Region'
+                  name='dataRegion'
+                  options={dataRegion}
+                  value={values.dataRegion}
+                />
+                {errors.dataRegion && touched.dataRegion && <Label basic color='red' pointing content={errors.dataRegion} />}
+              </Form.Field>
+              <Form.Field>
+                <Form.Select
+                  label='Listing Agent'
+                  name='listingAgent'
+                  options={listingAgent}
+                  value={values.listingAgent}
+                />
+                {errors.listingAgent && touched.listingAgent && <Label basic color='red' pointing content={errors.listingAgent} />}
+              </Form.Field>
+              <Form.Field>
+                <Form.Select
+                  label='User Type'
+                  name='userType'
+                  options={userType}
+                  value={values.userTypeId}
+                />
+                {errors.userType && touched.userType && <Label basic color='red' pointing content={errors.userType} />}
+              </Form.Field>
             </Form.Group>
             <Form.Group widths='equal'>
               <h4><b><div align='left'> Menu Access: </div></b></h4>
@@ -149,7 +280,66 @@ class NewUserModal extends Component {
 NewUserModal.propTypes = {
   user: PropTypes.object,
   modalOpen: PropTypes.bool,
-  funcao: PropTypes.func
+  funcao: PropTypes.func,
+  values: PropTypes.object,
+  touched: PropTypes.object,
+  errors: PropTypes.object,
+  handleChange: PropTypes.func,
+  handleBlur: PropTypes.func
 }
 
-export default NewUserModal
+const mapPropsToValues = () => ({
+  email: '',
+  password: '',
+  firstName: '',
+  lastName: '',
+  phoneHome: '',
+  phoneWork: '',
+  phoneMobile: '',
+  suburb: '',
+  street: '',
+  postCode: '',
+  listingAgent: '',
+  userType: ''
+})
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid email address.')
+    .required('Email is required.'),
+  password: Yup.string()
+    .required('Password is required.')
+    .min(4, 'Password required minimum 4 characters.')
+    .max(20, 'Password require max 20 characters.'),
+  firstName: Yup.string()
+    .required('First Name is required.')
+    .max(20, 'First Name require max 20 characters.'),
+  lastName: Yup.string()
+    .required('Last Name is required.')
+    .max(20, 'Last Name require max 20 characters.'),
+  phoneHome: Yup.string()
+    .max(20, 'Home Phone require max 20 characters.'),
+  phoneWork: Yup.string()
+    .max(20, 'Work Phone require max 20 characters.'),
+  phoneMobile: Yup.string()
+    .max(20, 'Mobile Phone require max 20 characters.'),
+  suburb: Yup.string()
+    .max(40, 'Suburb require max 40 charecters.'),
+  street: Yup.string()
+    .max(40, 'Street require max 40 charecters.'),
+  postCode: Yup.number()
+    .integer('Only numbers are permitted.'),
+  dataRegion: Yup.string()
+    .required('Office Region is required.'),
+  listingAgent: Yup.string()
+    .required('Listing Agent is required.'),
+  userType: Yup.string()
+    .required('User Type is required.')
+})
+
+const handleSubmit = ({ email, firstName }) =>
+  console.log(email, firstName)
+
+export default withFormik({ mapPropsToValues, validationSchema, handleSubmit })(
+  NewUserModal
+)
