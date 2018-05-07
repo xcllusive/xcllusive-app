f74049422fdf43b4b3aadeb7e4866d545a59525f
Cayo Bayestorff <bayesrox@gmail.com>
2018-05-07 10:56:09 +1000

    changed id to buyerId

--- src/services/api/buyerLog.js
+++ src/services/api/buyerLog.js
@@ -1,8 +1,8 @@
 import request from './'
 
-export const get = id =>
+export const get = buyerId =>
   request({
     method: 'get',
     url: '/buyer/log',
-    data: id
+    params: { buyerId }
   })
