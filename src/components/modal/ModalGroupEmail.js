be87bc45d83e1a85c4ea34903a905fe7da4d1b99
Cayo Bayestorff <cayobayestorff@Cayos-iMac.local>
2018-08-22 15:10:12 +1000

    fixed WillReceiveProp

--- src/components/modal/ModalGroupEmail.js
+++ src/components/modal/ModalGroupEmail.js
@@ -38,14 +38,8 @@ class ModalGroupEmail extends Component {
     this.props.getBuyersGroupEmail(this.props.businessId)
   }
 
-  static getDerivedStateFromProps (nextProps) {
-    if (
-      !nextProps.listGroupEmail.length &&
-      this.props.listGroupEmail !== nextProps.listGroupEmail
-    ) {
-      alert('This business has no buyers to send email!')
-      this.props.closeModal()
-    }
+  componentWillUnmount () {
+    this.setState({ controleMsg: true })
   }
 
   _handleConfirm = isConfirmed => {
@@ -58,9 +52,9 @@ class ModalGroupEmail extends Component {
 
   _removeFileUploaded = e => {
     if (this.timer) clearTimeout(this.timer)
-    this.setState({fileUpload: false})
+    this.setState({ fileUpload: false })
     this.props.setFieldValue('attachment', null)
-    this.timer = setTimeout(() => this.setState({fileUpload: true}), 10)
+    this.timer = setTimeout(() => this.setState({ fileUpload: true }), 10)
   }
 
   _handleFileUpload = e => {
@@ -73,8 +67,12 @@ class ModalGroupEmail extends Component {
   }
 
   _markAllcheckBoxArray = (e, { checked }) => {
-    this.setState(prevState => ({checkboxMarkAll: !prevState.checkboxMarkAll}))
-    return checked ? this.setState({array: this.props.listGroupEmail}) : this.setState({array: []})
+    this.setState(prevState => ({
+      checkboxMarkAll: !prevState.checkboxMarkAll
+    }))
+    return checked
+      ? this.setState({ array: this.props.listGroupEmail })
+      : this.setState({ array: [] })
   }
 
   _checkBoxArray = (e, { values }) => {
@@ -112,161 +110,185 @@ class ModalGroupEmail extends Component {
       <Modal open size="small" onClose={() => this._handleConfirm(false)}>
         <Modal.Header>{options.title}</Modal.Header>
         <Modal.Content>
-          <Form>
-            <Form.Group>
-              <Dimmer.Dimmable dimmed={isLoadingGroupEmail}>
-                <Dimmer inverted active={isLoadingGroupEmail}>
-                  <Loader>Loading</Loader>
-                </Dimmer>
-                <Form.Checkbox
-                  label="Mark All"
-                  name="markAll"
-                  checked={this.state.checkboxMarkAll}
-                  onChange={this._markAllcheckBoxArray}
-                />
-                <Table celled compact definition>
-                  <Table.Body>
-                    {listGroupEmail.map((groupEmail, index) => (
-                      <Table.Row key={index}>
-                        <Table.Cell collapsing>
-                          <Checkbox
-                            checked={this.state.array.some(item => item.id === groupEmail.id) }
-                            values={groupEmail}
-                            onChange={this._checkBoxArray}
-                          />
-                        </Table.Cell>
-                        <Table.Cell>
-                          {groupEmail.firstName} {groupEmail.lastName}
-                        </Table.Cell>
-                        <Table.Cell>{groupEmail.email}</Table.Cell>
-                        <Table.Cell>
-                          <Label color={groupEmail.isPending ? 'red' : 'green'}>
-                            {groupEmail.isPending ? 'Pending' : 'Done'}
-                          </Label>
-                        </Table.Cell>
-                      </Table.Row>
-                    ))}
-                  </Table.Body>
-                </Table>
-              </Dimmer.Dimmable>
-            </Form.Group>
-            <Form.Group>
-              <Form.Field width={16}>
-                <Form.Input
-                  label="Subject"
-                  name="subject"
-                  autoComplete="subject"
-                  value={values.subject}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-                {errors.subject &&
-                  touched.subject && (
-                  <Label
-                    basic
-                    color="red"
-                    pointing
-                    content={errors.subject}
+          <Dimmer.Dimmable dimmed={isLoadingGroupEmail}>
+            <Dimmer inverted active={isLoadingGroupEmail}>
+              <Loader>Loading</Loader>
+            </Dimmer>
+            {listGroupEmail.length ? (
+              <Form>
+                <Form.Group>
+                  <Form.Checkbox
+                    label="Mark All"
+                    name="markAll"
+                    checked={this.state.checkboxMarkAll}
+                    onChange={this._markAllcheckBoxArray}
                   />
-                )}
-              </Form.Field>
-            </Form.Group>
-            <Form.Group>
-              <h5 style={{ fontSize: '.92857143em' }}>Body</h5>
-            </Form.Group>
-            <Form.Group>
-              <Form.Field width={16}>
-                <Segment size="mini" inverted color="grey">
-                  <Header inverted>Dear ‹‹buyer`s name››</Header>
-                </Segment>
-                <Form.TextArea
-                  name="body"
-                  autoComplete="body"
-                  value={values.body}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-                <Segment size="mini" inverted color="grey">
-                  <Header inverted>
-                    Xcllusive Business Sales
-                    <p />
-                    www.xcllusive.com.au | (02) 9817 3331
-                  </Header>
-                </Segment>
-                {errors.text &&
-                  touched.text && (
-                  <Label basic color="red" pointing content={errors.text} />
-                )}
-              </Form.Field>
-            </Form.Group>
-            <Form.Group>
-              <Form.Field width={11}>
-                { this.state.fileUpload ? (
-                  <Form.Input
-                    type="file"
-                    label="Attachment"
-                    name="attachment"
-                    autoComplete="attachment"
-                    onChange={this._handleFileUpload}
-                  />
-                ) : null }
+                </Form.Group>
+                <Form.Group>
+                  <Table celled compact definition>
+                    <Table.Body>
+                      {listGroupEmail.map((groupEmail, index) => (
+                        <Table.Row key={index}>
+                          <Table.Cell collapsing>
+                            <Checkbox
+                              checked={this.state.array.some(
+                                item => item.id === groupEmail.id
+                              )}
+                              values={groupEmail}
+                              onChange={this._checkBoxArray}
+                            />
+                          </Table.Cell>
+                          <Table.Cell>
+                            {groupEmail.firstName} {groupEmail.lastName}
+                          </Table.Cell>
+                          <Table.Cell>{groupEmail.email}</Table.Cell>
+                          <Table.Cell>
+                            <Label
+                              color={groupEmail.isPending ? 'red' : 'green'}
+                            >
+                              {groupEmail.isPending ? 'Pending' : 'Done'}
+                            </Label>
+                          </Table.Cell>
+                        </Table.Row>
+                      ))}
+                    </Table.Body>
+                  </Table>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Field width={16}>
+                    <Form.Input
+                      label="Subject"
+                      name="subject"
+                      autoComplete="subject"
+                      value={values.subject}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.subject &&
+                      touched.subject && (
+                      <Label
+                        basic
+                        color="red"
+                        pointing
+                        content={errors.subject}
+                      />
+                    )}
+                  </Form.Field>
+                </Form.Group>
+                <Form.Group>
+                  <h5 style={{ fontSize: '.92857143em' }}>Body</h5>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Field width={16}>
+                    <Segment size="mini" inverted color="grey">
+                      <Header inverted>Dear ‹‹buyer`s name››</Header>
+                    </Segment>
+                    <Form.TextArea
+                      name="body"
+                      autoComplete="body"
+                      value={values.body}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    <Segment size="mini" inverted color="grey">
+                      <Header inverted>
+                        Xcllusive Business Sales
+                        <p />
+                        www.xcllusive.com.au | (02) 9817 3331
+                      </Header>
+                    </Segment>
+                    {errors.text &&
+                      touched.text && (
+                      <Label
+                        basic
+                        color="red"
+                        pointing
+                        content={errors.text}
+                      />
+                    )}
+                  </Form.Field>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Field width={11}>
+                    {this.state.fileUpload ? (
+                      <Form.Input
+                        type="file"
+                        label="Attachment"
+                        name="attachment"
+                        autoComplete="attachment"
+                        onChange={this._handleFileUpload}
+                      />
+                    ) : null}
 
-                {errors.attachment &&
-                  touched.attachment && (
-                  <Label
-                    basic
-                    color="red"
-                    pointing
-                    content={errors.attachment}
+                    {errors.attachment &&
+                      touched.attachment && (
+                      <Label
+                        basic
+                        color="red"
+                        pointing
+                        content={errors.attachment}
+                      />
+                    )}
+                  </Form.Field>
+                  <Form.Field width={5} style={{ alignSelf: 'flex-end' }}>
+                    <Form.Button
+                      floated="right"
+                      color="yellow"
+                      onClick={e => this._removeFileUploaded(e)}
+                    >
+                      <Icon name="remove" />
+                      Remove
+                    </Form.Button>
+                  </Form.Field>
+                </Form.Group>
+                <Form.Group>
+                  <Message info size="small">
+                    <p>
+                      You can attach one file. To send multiple files you can
+                      compress the files and attach.
+                    </p>
+                  </Message>
+                </Form.Group>
+                <Form.Group>
+                  <b>
+                    <label>
+                      Who would you like the recipients to reply to?
+                    </label>
+                  </b>
+                  <Form.Field
+                    control={Radio}
+                    label="You only"
+                    name="replyTo"
+                    onChange={this._handleChangeCheckBox}
+                    checked={values.replyTo}
                   />
-                )}
-              </Form.Field>
-              <Form.Field width={5} style={{ alignSelf: 'flex-end' }}>
-                <Form.Button
-                  floated="right"
-                  color="yellow"
-                  onClick={e => this._removeFileUploaded(e)}
-                >
-                  <Icon name="remove" />
-                  Remove
-                </Form.Button>
-              </Form.Field>
-            </Form.Group>
-            <Form.Group>
-              <Message info size="small">
-                <p>
-                  You can attach one file. To send multiple files you can
-                  compress the files and attach.
-                </p>
-              </Message>
-            </Form.Group>
-            <Form.Group>
+                  <Form.Field
+                    control={Radio}
+                    label="You and the office"
+                    name="replyTo"
+                    onChange={this._handleChangeCheckBox}
+                    checked={!values.replyTo}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <b>
+                    <label>
+                      Total email(s) to be generated: {this.state.array.length}
+                    </label>
+                  </b>
+                </Form.Group>
+              </Form>
+            ) : (
+              ''
+            )}
+            {!listGroupEmail.length && !isLoadingGroupEmail ? (
               <b>
-                <label>Who would you like the recipients to reply to?</label>
+                <label>This business has no buyers enquired! </label>
               </b>
-              <Form.Field
-                control={Radio}
-                label="You only"
-                name="replyTo"
-                onChange={this._handleChangeCheckBox}
-                checked={values.replyTo}
-              />
-              <Form.Field
-                control={Radio}
-                label="You and the office"
-                name="replyTo"
-                onChange={this._handleChangeCheckBox}
-                checked={!values.replyTo}
-              />
-            </Form.Group>
-            <Form.Group>
-              <b>
-                <label>
-                  Total email(s) to be generated: {this.state.array.length}
-                </label>
-              </b>
-            </Form.Group>
-          </Form>
+            ) : (
+              ''
+            )}
+          </Dimmer.Dimmable>
         </Modal.Content>
         <Modal.Actions>
           <Button
