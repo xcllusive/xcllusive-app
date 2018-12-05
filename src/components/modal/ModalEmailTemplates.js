028b2205494196c0a0a055bde81971592304bcac
Cayo Bayestorff <cayobayestorff@Cayos-iMac.local>
2018-12-05 15:44:46 +1100

    added buyer name when open email to send to buyer

--- src/components/modal/ModalEmailTemplates.js
+++ src/components/modal/ModalEmailTemplates.js
@@ -1,31 +1,16 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import {
-  Modal,
-  Button,
-  Form,
-  Grid,
-  Dimmer,
-  Loader,
-  Label,
-  Header
-} from 'semantic-ui-react'
+import { Modal, Button, Form, Dimmer, Loader, Header } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { closeModal } from '../../redux/ducks/modal'
 import { bindActionCreators } from 'redux'
 
 import { mapArrayToValuesForDropdownTemplates } from '../../utils/sharedFunctionArray'
 
-import {
-  getEmailTemplates,
-  getEmailTemplate,
-  clearEmailTemplates
-} from '../../redux/ducks/emailTemplates'
+import { getEmailTemplates, getEmailTemplate, clearEmailTemplates } from '../../redux/ducks/emailTemplates'
 
 import { sendEmailBuyerBrokersEmail } from '../../redux/ducks/buyer'
 
-import ReactQuill from 'react-quill'
-import 'react-quill/dist/quill.snow.css'
 import { withFormik } from 'formik'
 
 class ModalEmailTemplates extends Component {
@@ -37,12 +22,7 @@ class ModalEmailTemplates extends Component {
         toolbar: [
           [{ header: [1, 2, false] }],
           ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-          [
-            { list: 'ordered' },
-            { list: 'bullet' },
-            { indent: '-1' },
-            { indent: '+1' }
-          ],
+          [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
           ['link', 'image'],
           ['clean']
         ]
@@ -65,14 +45,9 @@ class ModalEmailTemplates extends Component {
     this.quillRef = null
   }
 
-  componentDidUpdate () {
-    this._attachQuillRefs()
-  }
-
   componentDidMount () {
     this.props.getEmailTemplates(true)
     this.props.clearEmailTemplates()
-    this._attachQuillRefs()
   }
 
   // static getDerivedStateFromProps (nextProps) {
@@ -117,26 +92,10 @@ class ModalEmailTemplates extends Component {
     // sendEmail.subject = this.props.values.subject
     // sendEmail.buyerId = parseInt(this.props.buyerId)
     // this.props.sendEmailBuyerBrokersEmail(sendEmail)
-    alert('Don`t worry! An e-mail app will be opened for you very shortly.')
-    window.location.href = `mailto:${this.props.email} ?subject=${
-      this.props.values.subject
-    } &body=${this._convertHtmlToRightText(this.props.values.body)}`
+    window.location.href = `mailto:${this.props.buyerObject.email} ?subject=${this.props.values.subject} &body=Hi ${
+      this.props.buyerObject.firstName
+    }, %0D%0A %0D%0A${this._convertHtmlToRightText(this.props.values.body)}`
     this.props.closeModal()
-  }
-
-  _attachQuillRefs = () => {
-    // Ensure React-Quill reference is available:
-    if (
-      !this.reactQuillRef ||
-      typeof this.reactQuillRef.getEditor !== 'function'
-    ) {
-      return false
-    }
-    // Skip if Quill reference is defined:
-    if (this.quillRef !== null) return false
-
-    const quillRef = this.reactQuillRef.getEditor()
-    if (quillRef !== null) this.quillRef = quillRef
   }
 
   render () {
@@ -146,11 +105,6 @@ class ModalEmailTemplates extends Component {
       objectEmailTemplate,
       isLoadingEmailTemplates,
       isLoadingTemplate,
-      values,
-      handleBlur,
-      handleChange,
-      touched,
-      errors,
       isLoadingSentEmail,
       isLoadingEmailTemplate
     } = this.props
@@ -165,9 +119,7 @@ class ModalEmailTemplates extends Component {
                   style={{ zIndex: 9999 }}
                   label="Templates"
                   placeholder="Please select one template bellow..."
-                  options={mapArrayToValuesForDropdownTemplates(
-                    listEmailTemplates
-                  )}
+                  options={mapArrayToValuesForDropdownTemplates(listEmailTemplates)}
                   name="title"
                   autoComplete="title"
                   onChange={this._handleSelectChange}
@@ -177,66 +129,14 @@ class ModalEmailTemplates extends Component {
               </Form.Field>
             </Form.Group>
             <Dimmer.Dimmable dimmed={!objectEmailTemplate || isLoadingTemplate}>
-              <Dimmer
-                inverted
-                active={!objectEmailTemplate || isLoadingTemplate}
-              >
-                {isLoadingTemplate ? (
-                  <Loader inverted />
-                ) : (
-                  <Header as="h2">Please, select one template!</Header>
-                )}
+              <Dimmer inverted active={!objectEmailTemplate || isLoadingTemplate}>
+                {isLoadingTemplate ? <Loader inverted /> : <Header as="h2">Please, select one template!</Header>}
               </Dimmer>
-              <Form.Group>
-                <Form.Field width={16}>
-                  <Form.Input
-                    readOnly
-                    label="Subject"
-                    name="subject"
-                    autoComplete="subject"
-                    value={values.subject}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  {errors.subject &&
-                    touched.subject && (
-                    <Label
-                      basic
-                      color="red"
-                      pointing
-                      content={errors.subject}
-                    />
-                  )}
-                </Form.Field>
-              </Form.Group>
-              <Grid padded="horizontally">
-                <Grid.Row columns={1}>
-                  <Grid.Column floated="left" width={16}>
-                    <Form.Field>
-                      <ReactQuill
-                        readOnly
-                        ref={el => {
-                          this.reactQuillRef = el
-                        }}
-                        value={values.body}
-                        onChange={this._handleChangeBody}
-                        style={{ height: '100%' }}
-                        modules={this.state.modules}
-                        formats={this.state.formats}
-                      />
-                    </Form.Field>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
             </Dimmer.Dimmable>
           </Form>
         </Modal.Content>
         <Modal.Actions>
-          <Button
-            negative
-            content="Cancel"
-            onClick={() => this._handleConfirm(false)}
-          />
+          <Button negative content="Cancel" onClick={() => this._handleConfirm(false)} />
           <Button
             positive
             icon="mail"
@@ -272,8 +172,7 @@ ModalEmailTemplates.propTypes = {
   errors: PropTypes.object,
   setFieldValue: PropTypes.func,
   sendEmailBuyerBrokersEmail: PropTypes.func,
-  buyerId: PropTypes.string,
-  email: PropTypes.string,
+  buyerObject: PropTypes.object,
   isLoadingSentEmail: PropTypes.bool,
   isSentEmail: PropTypes.bool,
   isLoadingEmailTemplate: PropTypes.bool
