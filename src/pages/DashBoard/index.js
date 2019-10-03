60b43fe0b88ebeaf230518597f11f27afbf5f04b
bayestorff <bayesrox@gmail.com>
2019-10-03 12:31:36 +1000

    dashboard message

--- src/pages/DashBoard/index.js
+++ src/pages/DashBoard/index.js
@@ -1,43 +1,105 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { withFormik } from 'formik'
 import { bindActionCreators } from 'redux'
 import { getUserLogged } from '../../redux/ducks/user'
-import { Grid } from 'semantic-ui-react'
+import { getSystemSettings } from '../../redux/ducks/systemSettings'
+import { Grid, Image, Message } from 'semantic-ui-react'
+import mercuryIcon from './mercury.png'
+// import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
 
 class DashBoardPage extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      modules: {
+        toolbar: [
+          [{ header: [1, 2, false] }],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+          ['link', 'image'],
+          ['clean']
+        ]
+      },
+
+      formats: [
+        'header',
+        'bold',
+        'italic',
+        'underline',
+        'strike',
+        'blockquote',
+        'list',
+        'bullet',
+        'indent',
+        'link',
+        'image'
+      ]
+    }
+  }
+
   componentDidMount () {
     this.props.getUserLogged()
+    this.props.getSystemSettings(1)
+  }
+
+  _createMarkup () {
+    return { __html: this.props.messageMercury }
   }
 
   render () {
+    // const { values } = this.props
     return (
       <Grid>
-        <Grid.Row style={{ marginTop: '200px' }}>
+        <Grid.Row>
+          <Grid.Column textAlign="center">
+            <Image src={mercuryIcon} style={{ marginTop: '50px' }} size="small" verticalAlign="middle" />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
           <Grid.Column textAlign="center">
             <h1 style={{ color: 'rgb(7, 85, 148)' }}>Hello {this.props.user ? this.props.user.firstName : null},</h1>
             <h1 style={{ color: 'rgb(7, 85, 148)' }}>Welcome to Mercury!</h1>
           </Grid.Column>
         </Grid.Row>
-        {/* <h1>DashBoard</h1> */}
+        {this.props.messageMercury ? (
+          <Grid.Row>
+            <Grid.Column>
+              <Message style={{ marginRight: '100px', marginLeft: '100px' }}>
+                <div dangerouslySetInnerHTML={this._createMarkup()} />
+              </Message>
+            </Grid.Column>
+          </Grid.Row>
+        ) : null}
       </Grid>
     )
   }
 }
 
 DashBoardPage.propTypes = {
+  values: PropTypes.object,
   getUserLogged: PropTypes.func,
-  user: PropTypes.object
+  user: PropTypes.object,
+  getSystemSettings: PropTypes.func,
+  messageMercury: PropTypes.string
 }
 
+const mapPropsToValues = props => ({
+  messageMercury: props.messageMercury ? props.messageMercury : ''
+})
+
 const mapStateToProps = state => ({
-  user: state.user.getLogged.object
+  user: state.user.getLogged.object,
+  messageMercury: state.systemSettings.get.object.messageMercury
 })
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
-      getUserLogged
+      getUserLogged,
+      getSystemSettings
     },
     dispatch
   )
@@ -45,4 +107,9 @@ const mapDispatchToProps = dispatch =>
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(DashBoardPage)
+)(
+  withFormik({
+    mapPropsToValues,
+    enableReinitialize: true
+  })(DashBoardPage)
+)
